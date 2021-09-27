bfa0d02355461955b38359d3e6d58439a3e90c72
KDot <jacob.joshua.cortez@icloud.com>
2021-09-26 19:08:04 -0500

    Adding folders from homework instructions

---
+++ names_activity.py
@@ -0,0 +1,10 @@
+### combine the idea
+my_name = input("What's your name?")
+friend_name = input("What's your friend's name?")
+
+### how long we have been conding
+name_coding = int(input("How many years have you been coding "+ my_name + "?"))
+friend_coding = int(input("How many years have you been coding "+ friend_name + "?"))
+
+### combine names and lenght of coding
+print("My name is " + my_name + " I have been coding for " + str(name_coding) + " years!" + "My name is " + friend_name + " I have been coding for " + str(friend_coding) + " years!")
